4a28ac8724ad20e839e561b13e963aa01b2f80e5
bioannidis <basilhsiwannidhs@windowslive.com>
2020-02-13 17:41:42 -0800

    Refining project organizing classes.

--- panrep/model.py
+++ panrep/model.py
@@ -4,13 +4,14 @@ import dgl.function as fn
 
 class PanRepRGCN(nn.Module):
     def __init__(self, num_nodes, h_dim, inp_dim,out_dim, num_rels, num_bases,
-                 num_hidden_layers=1, dropout=0,
+                 num_hidden_layers=1, dropout=0, reconstruct_dim=1,
                  use_self_loop=False, use_cuda=False):
         super(PanRepRGCN, self).__init__()
         self.num_nodes = num_nodes
         self.h_dim = h_dim
         self.inp_dim=inp_dim
         self.out_dim = out_dim
+        self.reconstruct_dim=reconstruct_dim
         self.num_rels = num_rels
         self.num_bases = None if num_bases < 0 else num_bases
         self.num_hidden_layers = num_hidden_layers
@@ -49,7 +50,7 @@ class PanRepRGCN(nn.Module):
         for layer in self.layers:
             h = layer(g, h, r, norm)
         # TODO write more neatly this layer performs attribute reconstruction
-        m=nn.Linear(self.h_dim,1)
+        m=nn.Linear(self.h_dim,self.reconstruct_dim)
         reconstructed=m(h)
 
         return reconstructed.squeeze(),h
@@ -101,3 +102,42 @@ class BaseRGCN(nn.Module):
         for layer in self.layers:
             h = layer(g, h, r, norm)
         return h
+'''
+
+class PanRepRGCN(nn.Module):
+    def __init__(self, num_nodes, h_dim, inp_dim,out_dim, num_rels, num_bases,
+                 num_hidden_layers=1, dropout=0,
+                 use_self_loop=False, use_cuda=False):
+        super(PanRepRGCN, self).__init__()
+        self.num_nodes = num_nodes
+        self.h_dim = h_dim
+        self.inp_dim=inp_dim
+        self.out_dim = out_dim
+        self.num_rels = num_rels
+        self.num_bases = None if num_bases < 0 else num_bases
+        self.num_hidden_layers = num_hidden_layers
+        self.dropout = dropout
+        self.use_self_loop = use_self_loop
+        self.use_cuda = use_cuda
+        self.baseRGCN=BaseRGCN(num_nodes, h_dim,inp_dim, out_dim, num_rels, num_bases,
+                 num_hidden_layers, dropout,use_self_loop, use_cuda)
+        # create rgcn layers
+        self.baseRGCN.build_model()
+
+    def build_input_layer(self):
+        return None
+
+    def build_hidden_layer(self, idx):
+        raise NotImplementedError
+
+    def build_output_layer(self):
+        return None
+
+    def forward(self, g, h, r, norm):
+        h=self.baseRGCN(g,h,r,norm)
+        # TODO write more neatly this layer performs attribute reconstruction
+        m=nn.Linear(self.h_dim,1)
+        reconstructed=m(h)
+
+        return reconstructed.squeeze(),h
+'''
