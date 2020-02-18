8635f7bdd682b68ec8b7578b678062182ce5e090
bioannidis <basilhsiwannidhs@windowslive.com>
2020-02-17 18:15:34 -0800

    Code structure refactoring

--- panrep/entity_classify.py
+++ panrep/entity_classify.py
@@ -15,104 +15,18 @@ import torch
 import torch.nn.functional as F
 from dgl import DGLGraph
 from classifiers import ClassifierRGCN,ClassifierMLP
+from load_data import load_db_data, load_gen_data
 from model import PanRepRGCN,PanRepRGCNHetero
 from sklearn.metrics import roc_auc_score
-from dgl.contrib.data import load_data
 from node_supervision_tasks import node_attribute_reconstruction
-import random
-import os
-import pickle
-from sklearn.model_selection import StratifiedShuffleSplit
-from dgl.data.rdf import AIFB, MUTAG, BGS, AM
 
-def load_db_data(args):
-    use_cuda = 0;
-    check_cuda = torch.cuda.is_available()
-    if use_cuda < 0:
-        check_cuda = False;
-    device = torch.device("cuda:" + str(use_cuda) if check_cuda else "cpu")
-    print("Using device", device)
-    cpu_device = torch.device("cpu");
-
-    # In[10]:
-
-    seed = 0;
-    np.random.seed(seed)
-    random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed)
-    torch.backends.cudnn.deterministic = True
-
-    # In[12]:
-
-    data_folder = "../data/"
-
-    # In[13]:
-
-    G = pickle.load(open(os.path.join(data_folder, 'graph_0.001.pickle'), "rb")).to(device)
-
-    # In[14]:
-
-    labels = pickle.load(open(os.path.join(data_folder, 'labels.pickle'), "rb"))
-
-    # In[15]:
-
-    print(G)
-
-    # In[16]:
-
-    # G.nodes['application'].data['features'].fill_(0.0);
-
-    # In[17]:
-
-    print(labels)
-
-    # In[18]:
-
-    label_indices = [i for i in range(len(labels))];
-    sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=seed);
-    train_idx, test_idx = next(sss.split(label_indices, labels));
-
-    sss = StratifiedShuffleSplit(n_splits=1, test_size=0.5, random_state=seed);
-    valid_index_temp, test_index_temp = next(sss.split(list(test_idx), np.array(labels)[test_idx]));
-    val_idx = np.array(test_idx)[valid_index_temp];
-    test_idx = np.array(test_idx)[test_index_temp];
-
-    train_idx = np.array(train_idx);
-    test_idx = np.array(test_idx);
-    val_idx = np.array(val_idx);
-    category='history'
-    num_classes=1
-    return train_idx,test_idx,val_idx,labels,G,category,num_classes
-
-# TODO try the reconstruction loss
-def load_gen_data(args):
-    data = load_data(args.dataset, bfs_level=args.bfs_level, relabel=args.relabel)
-    num_nodes = data.num_nodes
-    num_rels = data.num_rels
-    num_classes = data.num_classes
-    labels = data.labels
-    train_idx = data.train_idx
-    test_idx = data.test_idx
-
-    # split dataset into train, validate, test
-    if args.validation:
-        val_idx = train_idx[:len(train_idx) // 5]
-        train_idx = train_idx[len(train_idx) // 5:]
-    else:
-        val_idx = train_idx
-
-    # since the nodes are featureless, the input feature is then the node id.
-    feats = torch.arange(num_nodes)
-    return num_nodes,num_rels,num_classes,train_idx,test_idx,val_idx,labels,feats,data.edge_type,data.edge_norm,data.edge_src,data.edge_dst
 
 def main(args):
     rgcn_hetero(args)
 
 def rgcn_hetero(args):
     if args.dataset == "database_data":
-        train_idx,test_idx,val_idx,labels,g,category,num_classes=load_db_data(args)
+        train_idx,test_idx,val_idx,labels,g,category,num_classes= load_db_data(args)
     else:
         raise NotImplementedError
 
@@ -238,7 +152,7 @@ def rgcn(args):
         raise NotImplementedError
     else:
         num_nodes, num_rels, num_classes, train_idx,test_idx,\
-        val_idx,labels, feats,edge_type,edge_norm,edge_src,edge_dst=load_gen_data(args)
+        val_idx,labels, feats,edge_type,edge_norm,edge_src,edge_dst= load_gen_data(args)
 
     # edge type and normalization factor
     edge_type = torch.from_numpy(edge_type)
---
+++ panrep/load_data.py
@@ -0,0 +1,91 @@
+import os
+import pickle
+import random
+
+import numpy as np
+import torch
+from dgl.contrib.data import load_data
+from sklearn.model_selection import StratifiedShuffleSplit
+
+
+def load_db_data(args):
+    use_cuda = 0;
+    check_cuda = torch.cuda.is_available()
+    if use_cuda < 0:
+        check_cuda = False;
+    device = torch.device("cuda:" + str(use_cuda) if check_cuda else "cpu")
+    print("Using device", device)
+    cpu_device = torch.device("cpu");
+
+    # In[10]:
+
+    seed = 0;
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    torch.backends.cudnn.deterministic = True
+
+    # In[12]:
+
+    data_folder = "../data/"
+
+    # In[13]:
+
+    G = pickle.load(open(os.path.join(data_folder, 'graph_0.001.pickle'), "rb")).to(device)
+
+    # In[14]:
+
+    labels = pickle.load(open(os.path.join(data_folder, 'labels.pickle'), "rb"))
+
+    # In[15]:
+
+    print(G)
+
+    # In[16]:
+
+    # G.nodes['application'].data['features'].fill_(0.0);
+
+    # In[17]:
+
+    print(labels)
+
+    # In[18]:
+
+    label_indices = [i for i in range(len(labels))];
+    sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=seed);
+    train_idx, test_idx = next(sss.split(label_indices, labels));
+
+    sss = StratifiedShuffleSplit(n_splits=1, test_size=0.5, random_state=seed);
+    valid_index_temp, test_index_temp = next(sss.split(list(test_idx), np.array(labels)[test_idx]));
+    val_idx = np.array(test_idx)[valid_index_temp];
+    test_idx = np.array(test_idx)[test_index_temp];
+
+    train_idx = np.array(train_idx);
+    test_idx = np.array(test_idx);
+    val_idx = np.array(val_idx);
+    category='history'
+    num_classes=1
+    return train_idx,test_idx,val_idx,labels,G,category,num_classes
+
+
+def load_gen_data(args):
+    data = load_data(args.dataset, bfs_level=args.bfs_level, relabel=args.relabel)
+    num_nodes = data.num_nodes
+    num_rels = data.num_rels
+    num_classes = data.num_classes
+    labels = data.labels
+    train_idx = data.train_idx
+    test_idx = data.test_idx
+
+    # split dataset into train, validate, test
+    if args.validation:
+        val_idx = train_idx[:len(train_idx) // 5]
+        train_idx = train_idx[len(train_idx) // 5:]
+    else:
+        val_idx = train_idx
+
+    # since the nodes are featureless, the input feature is then the node id.
+    feats = torch.arange(num_nodes)
+    return num_nodes,num_rels,num_classes,train_idx,test_idx,val_idx,labels,feats,data.edge_type,data.edge_norm,data.edge_src,data.edge_dst
